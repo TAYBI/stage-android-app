a26e6582fabb88b824edf1689136a8d2a9d180d0
TAYBI <taybib4@gmail.com>
2020-05-04 17:09:46 +0000

    login ui

--- app/src/main/java/com/example/ofppt2/OfpptOpenHelper.java
+++ app/src/main/java/com/example/ofppt2/OfpptOpenHelper.java
@@ -3,12 +3,12 @@ package com.example.ofppt2;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import androidx.annotation.Nullable;
+import android.support.annotation.Nullable;
 
 public class OfpptOpenHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Inscription.db";
     public static final int DATABASE_VERSION = 1;
+
 
     public OfpptOpenHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
