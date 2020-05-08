a24726a44912e746cff2f19457fb8c8152c97fea
TAYBI <taybib4@gmail.com>
2020-05-08 15:51:13 +0000

    DataMager class - select secteur par niveau - select filier par secteur

---
+++ app/src/main/java/com/example/ofppt2/DataManager.java
@@ -0,0 +1,58 @@
+package com.example.ofppt2;
+
+import com.example.ofppt2.classes.*;
+
+import java.util.ArrayList;
+
+public class DataManager {
+    ArrayList<Filiere> Filieres;
+    ArrayList<Secteur> Secteurs;
+    ArrayList<Niveau> Niveaux;
+
+    public DataManager() {
+        Filieres = new ArrayList<Filiere>();
+        Secteurs = new ArrayList<Secteur>();
+        Niveaux = new ArrayList<Niveau>();
+    }
+
+    public String getFilieres(String secteur) {
+        for(Secteur secteurItem: Secteurs){
+            if (secteur.equals(secteurItem.getName())){
+                return secteurItem.getFilieres();
+            }
+        }
+        return null;
+    }
+
+    public String getSecteurs(String niveau)
+    {
+        for(Niveau niveauItem: Niveaux){
+            if (niveau.equals(niveauItem.getName())){
+                return niveauItem.getSecteurs();
+            }
+        }
+        return null;
+    }
+
+    public Filiere getFiliere(String filiereName) {
+        for(Filiere filiere: Filieres){
+            if (filiereName.equals(filiere.getNomModule())){
+                return filiere;
+            }
+        }
+        return null;
+    }
+
+
+    public void setFiliere(Filiere filiere) {
+        Filieres.add(filiere);
+    }
+
+    public void setSecteur(Secteur secteur) {
+        Secteurs.add(secteur);
+    }
+
+    public void setNiveau(Niveau niveau) {
+        Niveaux.add(niveau);
+    }
+}
--- app/src/main/java/com/example/ofppt2/MainActivity.java
+++ app/src/main/java/com/example/ofppt2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ofppt2;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.ofppt2.classes.Niveau;
+import com.example.ofppt2.classes.Secteur;
 import com.example.ofppt2.data.OfpptOpenHelper;
 
 import java.util.Arrays;
@@ -15,17 +19,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
+    DataManager dm;
     OfpptOpenHelper db;
     Spinner mSpinnerNiveauFormation, mSpinnerNiveauScolaire, mSpinnerSecteur, mSpinnerFiliere;
-
-    private String niveaFormation, secteur, filiere;
+    private String niveaFormation, secteurs, filieres;
     private String niveauScolaire = "bac,niveau bac";
 
-//    private String secteur = "Bâtiment et Travaux Publics,Froid et Génie Thermique," +
-//                            "Réparation des Engins à Moteurs," +
-//                            "Génie Electrique,Plasturgie,Administration Gestion et Commerce," +
-//                            "Construction Métallique,Transport et Logistique,Textile Habillement,"+
-//                            "Action Sociale,NTIC,Arts Graphiques,Aéronautique";
 
             @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +32,69 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         db = new OfpptOpenHelper(this);
+        dm = new DataManager();
 
         mSpinnerNiveauFormation = (Spinner)findViewById(R.id.spinner_niveau_formation);
         mSpinnerSecteur = (Spinner)findViewById(R.id.spinner_secteur);
         mSpinnerFiliere = (Spinner)findViewById(R.id.spinner_filiere);
-
         mSpinnerNiveauScolaire = (Spinner)findViewById(R.id.spinner_niveau_scolaire);
         fillSpinner(mSpinnerNiveauScolaire, stringToList(niveauScolaire));
 
+        load_Niveaux_data();
+        load_Secteurs_data();
+
         load_Niveau_Formation_ParNiveau_Scolaire();
         load_Secteur_Par_Niveau_Formation();
+        load_Filiere_Par_Secteur();
+    }
+
+    private void load_Niveaux_data() {
+        Cursor res =  db.getAllNiveaux();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found");
+        }
+        while (res.moveToNext()){
+            Niveau niveau = new Niveau(res.getString(1), res.getString(2));
+            dm.setNiveau(niveau);
+        }
+
+    }
+
+
+    private void load_Secteurs_data() {
+        Cursor res =  db.getAllSecteurs();
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found");
+        }
+        while (res.moveToNext()){
+            Secteur secteur = new Secteur(res.getString(1), res.getString(2));
+            dm.setSecteur(secteur);
+        }
+    }
+
+    private void load_Filiere_Par_Secteur() {
+        mSpinnerSecteur.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String secteur  = parent.getItemAtPosition(position).toString();
+                filieres = dm.getFilieres(secteur);
+                fillSpinner(mSpinnerFiliere, stringToList(filieres));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private void load_Secteur_Par_Niveau_Formation() {
         mSpinnerNiveauFormation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                
+                String niveau  = parent.getItemAtPosition(position).toString();
+                secteurs = dm.getSecteurs(niveau);
+                fillSpinner(mSpinnerSecteur, stringToList(secteurs));
             }
 
             @Override
@@ -91,4 +136,11 @@ public class MainActivity extends AppCompatActivity{
         spinner.setAdapter(adapter);
     }
 
+    private void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
 }
